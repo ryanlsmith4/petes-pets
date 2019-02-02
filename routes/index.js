66a92e28579a216fe3a66f2941e8b5a4756c3d36
Ryan Smith <ryan.l.smith4@gmail.com>
2019-02-01 17:45:57 -0800

    fixed tabulation

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+/*eslint consistent-return: false*/
 const Pet = require('../models/pet');
 
 module.exports = (app) => {
@@ -5,13 +6,12 @@ module.exports = (app) => {
   app.get('/', (req, res) => {
     const page = req.query.page || 1;
     // Pet.find().exec((err, pets)
-    Pet.paginate().then((results) => {
+    Pet.paginate({}, { limit: 3, page }).then((results) => {
       // console.log(results);
-      if (req.header('content-type') == 'application/json') {
-        return res.json({ pets: results.docs, pagesCount: results.pages, currentPage: page });
-      } else {
-        res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
+      if (req.header('content-type') === 'application/json') {
+        return res.json({ results });
       }
+      res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
     });
   });
 };
